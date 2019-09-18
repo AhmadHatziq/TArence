2f2f1876d9bb1004d773856b1a19c3f4aaabaf73
AhmadHatziq <e0031550@u.nus.edu>
2019-09-19 02:03:44 +0800

    Ported over files leftover from previous Tutorial 3

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+public class RemarkCommandParser {
+
+    public RemarkCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+
+        // Obtains the remark word ie r/"word"
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
+                PREFIX_REMARK);
+
+        // Obtains the index
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    RemarkCommand.MESSAGE_USAGE), ive);
+        }
+
+        String remarkString = argMultimap.getValue(PREFIX_REMARK).orElse("");
+        Remark remark = new Remark(remarkString);
+        return new RemarkCommand(index, remark);
+    }
+
+}
